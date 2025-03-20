83ce2587615c0336584b641650c430e0ab4ae927
shinigl <kraniket754@gmail.com>
2025-03-20 20:53:52 +0530

    Updated routes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -147,4 +147,19 @@ const getFeedPosts = async(req,res)=>{
         res.status(500).json({message:err.message})
     }
 }
-export {createPost,getPost,deletePost,likeUnlikePost, replyToPost, getFeedPosts} ;
+
+const getUserPosts = async(req,res)=>{
+    const {username} = req.params ;
+    try{
+       const user = await User.findOne({username});
+       if(!user){
+            return res.status(404).json({error:"User not found"}) ;
+       }
+       const posts = await Post.find({postedBy:user._id}).sort({createdAt: -1 });
+       res.status(200).json(posts) ;
+    }
+    catch(err){
+        res.status(500).json({error:err.message})
+    }
+}
+export {createPost,getPost,deletePost,likeUnlikePost, replyToPost, getFeedPosts,getUserPosts} ;
