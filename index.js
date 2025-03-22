4ccdd28537a1febe4021fc9ac875f7a48985a0af
shinigl <kraniket754@gmail.com>
2025-03-23 03:32:31 +0530

    .

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ cloudinary.config({
 
 //Middlewares
 app.use(express.json({limit:"50mb"})) //Parses data coming from req.body
-app.use(express.urlencoded({extended: true}))// To parse nested data from req.body
+app.use(express.urlencoded({extended: true})) // To parse nested data from req.body
 app.use(cookieParser());
 
 //Routes
